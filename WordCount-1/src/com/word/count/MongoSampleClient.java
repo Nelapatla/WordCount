15d77bb1ed5c728c0ffcba39d2b1aba4ee1a94fa
User <User@192.168.0.6>
2017-10-09 12:44:47 +0530

    modification for handle text,doc and exception handling

--- WordCount-1/src/com/word/count/MongoSampleClient.java
+++ WordCount-1/src/com/word/count/MongoSampleClient.java
@@ -1,22 +1,29 @@
 package com.word.count;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import org.apache.commons.io.FilenameUtils;
 import org.bson.BsonDocument;
 import org.bson.BsonInt32;
 import org.bson.BsonValue;
 import org.bson.Document;
 import org.bson.conversions.Bson;
-import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
@@ -30,57 +37,40 @@ import com.mongodb.client.model.Filters;
 
 public class MongoSampleClient extends DefaultHandler {
 
-	private String temp;
-	private String[] words;
 	private String wordKey = "word";
-	private boolean flag = true;
-	private int freq = 0;
 	private MongoClient mongoClient = null;
 	private MongoDatabase db = null;
 	private MongoCollection<Document> table = null;
-
-	public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
+	private String file = null;
+	private String dbFileName=null;
+	
+	public static void main(String[] args) {
 		MongoSampleClient handler = new MongoSampleClient();
-		String file = args[0];
-		String address[] = args[1].split(":");
-		String ipAddress=address[0];
-		int port = Integer.parseInt(address[1]);
-		handler.mongoClient = new MongoClient(ipAddress, port);
-		handler.db = handler.mongoClient.getDatabase("wordcount");
-		SAXParserFactory spfac = SAXParserFactory.newInstance();
-		SAXParser sp = spfac.newSAXParser();
-		sp.parse(file, handler);
-		handler.minFrequency();
-		handler.maxFrequency();
-
+		handler.prepareExecute(handler, args);
 	}
 
+	@Override
 	public void characters(char[] buffer, int start, int length) {
-		temp = new String(buffer, start, length);
-		frequencyCount(temp.trim());
-	}
-
-	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-	}
-
-	public void endElement(String uri, String localName, String qName) throws SAXException {
-
+		String temp = new String(buffer, start, length);
+		frequencyCount(temp);
 	}
 
 	private void frequencyCount(String temp) {
-		temp = temp.replaceAll("[^a-zA-Z0-9]", " ");
-		words = temp.split(" ");
+		//Remove all special character 
+		Pattern p = Pattern.compile("[^a-zA-Z0-9]");
+		String[] words = p.matcher(temp).replaceAll(" ").split(" ");
 		for (String s : words) {
-			String a = s.trim();
-			calldb(a);
-
+			if (!s.equals(null))
+				calldb(s.trim());
 		}
 	}
 
 	public void calldb(String s) {
-		try {
-			table = db.getCollection("w9");
+		boolean flag=true;
+		int freq = 0;
 
+		try {
+			table = db.getCollection(dbFileName);
 			BasicDBObject fields = new BasicDBObject();
 			List<BasicDBObject> searchArguments = new ArrayList<BasicDBObject>();
 			searchArguments.add(new BasicDBObject(s.toLowerCase(), new BasicDBObject("$eq", 1)));
@@ -90,7 +80,6 @@ public class MongoSampleClient extends DefaultHandler {
 
 			FindIterable<Document> it = table.find(fields);
 			MongoCursor<Document> iterator = it.iterator();
-
 			while (iterator.hasNext()) {
 				freq = iterator.next().getInteger(s.toLowerCase());
 				if (freq > 0) {
@@ -108,65 +97,133 @@ public class MongoSampleClient extends DefaultHandler {
 				table.insertOne(doc);
 
 			}
-			flag = true;
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
+	//Prepare for executing application
+	public void prepareExecute(MongoSampleClient handler, String args[]) {
+		String address[] = null;
+		String ipAddress = null;
+		@SuppressWarnings("resource")
+		Scanner scanner=new Scanner(System.in);
+		int port = 0;
+		if (args.length < 2) {
+			System.err.println("please provide valid input :");
+			System.out.println();
+			System.out.println("You want to continue press: Y else press N");
+			String choice=scanner.nextLine();
+			if(choice.equalsIgnoreCase("y")){
+			System.out.println("Enter file Name with complite path :");
+			file=scanner.nextLine();
+			System.out.println("Enter  server ip address:");
+			ipAddress=scanner.nextLine();
+			System.out.println("Enter  server port number:");
+			port=scanner.nextInt();
+			}else{
+				System.exit(0);
+			}
+		} else {
+			file = args[0];
+			address = args[1].split(":");
+			if (address.length >= 2) {
+				ipAddress = address[0];
+				port = Integer.parseInt(address[1]);
+			}
+		}
 
+		String fileExtension = FilenameUtils.getExtension(file);
+		dbFileName = FilenameUtils.getBaseName(file);
+		Path filePath = Paths.get(file);
+		if (Files.exists(filePath)) {
+			
+			System.out.println("Select path    : "+filePath);
+			System.out.println("Select file    : "+FilenameUtils.getName(file));
+			System.out.println("Selected server: "+ipAddress+":"+port);
+			System.out.println("File in table  : "+dbFileName);
+			
+			//call for Creating  connection
+			createDBConnection(ipAddress, port);
+			if (!fileExtension.equals("xml")) { //for doc, text file
+				try (Stream<String> lines = Files.lines(filePath, Charset.defaultCharset())) {
+					lines.forEachOrdered(line -> frequencyCount(line));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} else {   //for XML file 
+				SAXParserFactory spfac = SAXParserFactory.newInstance();
+				SAXParser sp;
+				try {
+					sp = spfac.newSAXParser();
+					sp.parse(file, handler);
+				} catch (ParserConfigurationException | SAXException | IOException e) {
+					e.printStackTrace();
+				}
+
+			}
+			 handler.minFrequency();
+			 handler.maxFrequency();
+		} else {
+			System.err.println("File does not exist :"+filePath);
+		}
+	}
+   //find least number of frequency in file
 	public void minFrequency() {
 		BsonValue b = new BsonInt32(+1);
 		Bson sort = new BsonDocument(wordKey, b);
 		Document min = table.find().sort(sort).first();
-		Map<String, Integer> minFreq = getWord(min);
-		
-		BsonValue c = new BsonInt32(minFreq.get(wordKey));
+		BsonValue c = new BsonInt32(getWord(min).get(wordKey));
 		Bson minFq = new BsonDocument(wordKey, c);
 		FindIterable<Document> find = table.find(minFq);
-		minFreq=getAllWord(find);
-		minFreq.forEach((key, value) -> System.out.println("least freaquency : " + key + " : " + value));
+		getAllWord("least freaquency",find);
 	}
-
+	//find Most number of frequency in file
 	public void maxFrequency() {
+		//Search Argument for  word have max frequency
 		BsonValue b = new BsonInt32(-1);
 		Bson sort = new BsonDocument(wordKey, b);
 		Document max = table.find().sort(sort).first();
 		
-		Map<String, Integer> maxFreq = getWord(max);
-		BsonValue c = new BsonInt32(maxFreq.get(wordKey));
+		//Search Argument for all word have same frequency 
+		BsonValue c = new BsonInt32(getWord(max).get(wordKey));
 		Bson maxf = new BsonDocument(wordKey, c);
 		FindIterable<Document> find = table.find(maxf);
-		maxFreq=getAllWord(find);
-		maxFreq.forEach((key, value) -> System.out.println("Max freaquency : " + key + " : " + value));
+		getAllWord("Max freaquency", find);
 	}
-
-	public Map<String, Integer> getAllWord(FindIterable<Document> find) {
+	// Find All Word Have a same frequency
+	public Map<String, Integer> getAllWord(String callingMethod,FindIterable<Document> find) {
 		MongoCursor<Document> iterator = find.iterator();
 		Map<String, Integer> count = new HashMap<>();
-		Document document =null;
-		Iterator<String> key=null;
-		String wk=null;
-		while(iterator.hasNext()){
-		document = iterator.next();
-		key = document.keySet().iterator();
-		while (key.hasNext()) {
-			wk = key.next();
-			if (!(wk.equals(wordKey)||wk.equals("_id")))
-				count.put(wk, document.getInteger(wk));
-		}
+		Document document = null;
+		Iterator<String> key = null;
+		String wk = null;
+		while (iterator.hasNext()) {
+			document = iterator.next();
+			key = document.keySet().iterator();
+			while (key.hasNext()) {
+				wk = key.next();
+				if (!(wk.equals(wordKey) || wk.equals("_id")))
+					System.out.println(callingMethod+" : "+ wk + " : " + document.getInteger(wk));
+			}
 		}
 		return count;
 	}
+	//Set Key and Value for Least or Most Used word 
 	public Map<String, Integer> getWord(Document document) {
 		Map<String, Integer> count = new HashMap<>();
 		Iterator<String> key = document.keySet().iterator();
-		String wk=null;
+		String wk = null;
 		while (key.hasNext()) {
 			wk = key.next();
 			if (!(wk.equals("_id")))
 				count.put(wk, document.getInteger(wk));
 		}
 		return count;
+	}
+	//creating DB connection
+	public void createDBConnection(String ipAddress, int port) {
+		mongoClient = new MongoClient(ipAddress, port);
+		db = mongoClient.getDatabase("wordcount");
 	}
 
 }
